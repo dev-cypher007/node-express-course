71bfdaa990108e963a91bb17ac9d01b5b8222827
dev-cypher007 <dev.cypher87@gmail.com>
2020-07-20 12:10:08 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const mochUserData = [
+    { name: 'Mark' },
+    { name: 'Jill'}
+]
+app.get('/users', function (req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users:mochUserData
+    })
+})
+
 app.listen(8000, function () {
     console.log("Server is running")
 })
